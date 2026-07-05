431d4fe3a96b17dd13fc839920322f05e26a215a
park122103-hue <park122103@gmail.com>
2026-07-05 17:36:35 +0900

    Update p016-wrong.cpp

--- p016-wrong.cpp
+++ p016-wrong.cpp
@@ -2,7 +2,9 @@
 #include <vector>
 
 using namespace std;
-
+//버블정렬의 정렬 횟수+1을 구하는 프로그램... 을 만드려고 했으나
+//아마도 이건 선택 정렬의 정렬 횟수+1을 구하는 프로그램인 듯 하다
+//정확히는 n부터 1까지 스캔한다음 최댓값을 n으로 보내는 선택정렬의 swap 횟수
 int main() {
     int n;
     cin >> n;   // 수열 길이
@@ -14,10 +16,10 @@ int main() {
     }
 
     int count = n;
-    int lager = A[n - 1];
+    int larger = A[n - 1];
     for (int i = n - 1; i > 0; i--) {
-        if (lager < A[i - 1]) {
-            lager = A[i - 1];
+        if (larger < A[i - 1]) {
+            larger = A[i - 1];
             count--;
         }
     }
