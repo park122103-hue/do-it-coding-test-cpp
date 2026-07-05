1b202c13fd7c4709b6a46c8da05b832ce8629ba4
park122103-hue <park122103@gmail.com>
2026-07-05 12:23:23 +0900

    Update and rename p003 to p003.cpp

--- p003.cpp
+++ p003.cpp
@@ -4,18 +4,20 @@
 using namespace std;
 
 int main() {
-    int n;
-    string str;
+    int n;    //수열 길이
+    string str;    //문자열(수열)
     cin >> n;
     cin >> str;
     int A[100];
-
+    //입력받은 문자열(수열)을 숫자로 변환해서 배열 A[i]에 저장
     for (int i = 0; i < n; i++) {
         A[i] = str[i] - '0';
     }
-
+    //구간합 수열 S[i] 정의
     int S[100];
+    //점화식의 초기 조건 S[0]=A[0]
     S[0] = A[0];
+    //점화식의 관계 식 S[i]=A[i]+S[i-1]
     for (int i = 1; i < n; i++) {
         S[i] = A[i] + S[i - 1];
     }
