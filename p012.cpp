184414b9f238abadb9d4c348e660407f2cf2044a
park122103-hue <park122103@gmail.com>
2026-07-05 16:27:38 +0900

    Update p012.cpp(버그수정 및 주석 추가)
    반복문의 조건으로 i<s.size()를 써서 스택이 빌 때까지 pop하려 했는데, 그러면 s.size()가 계속 바뀌는 논리 허점이 있었다. 따라서 !s.empty()를 사용하여 빌 때까지 반복하도록 수정하였다.

--- p012.cpp
+++ p012.cpp
@@ -7,28 +7,38 @@ using namespace std;
 typedef struct{
   int index, data;
 }NODE;
-//수열 A(i)에서 A(i)보다 큰 오른쪽에 있는 수중 가장 왼쪽에 있는 수(오큰수)를 구하는 프로그램
+//수열 A(i)에서 A(i)의 오큰수를 구하는 프로그램
+//오큰수: A(i)보다 큰 오른쪽에 있는 수중 가장 왼쪽에 있는 수이고, A(i)보다 큰 오른쪽에 있는 수가 없다면 오큰수는 -1이다
 int main() {
-    int n;
+    int n;  //수열 길이
     cin >> n;
-    stack<NODE> s;
-    vector<int> NGE(n);
-
+    stack<NODE> s;  //오큰수 대기열
+    vector<int> NGE(n);  //NGE(i)=A(i)의 오큰수
+  //규칙 1. 수열 A를 순서대로 스택에 넣는다
+  //규칙 2. A(top)<A(i)이면 A(top)를 스택에서 제거한다
+  //q1. A(0)보다 큰 수가 들어오면 A(0)은 제거될까(오큰수의 조건)?
+  //a. A(0)보다 큰 수가 최초로 들어왔다면
+  //b. A(0)보다 큰 수가 A(0) 바로 위로 들어오면 규칙 2에 의해 A(0)은 제거된다
+  //c. A(0)보다 큰 수가 들어왔을 때 A(0)위에 A(i), ... , A(j)이 있었다면
+  //d. A(0)보다 큰 수가 최초로 들어왔으므로 이전에 들어온 A(i), ... , A(j)는 A(0)보다 작다
+  //e. 따라서 큰 수는 스택 안의 모든 수보다 크므로 스택 안의 모든 수는 순차적으로 제거된다
+  //a1. 따라서 q1의 대답은 '그렇다' 이다.
     for (int i = 0; i < n; i++) {
         int now;
-        cin >> now;
+        cin >> now;  //now=A(i)
         while (!s.empty() && s.top().data < now) {
             NGE[s.top().index] = now;
             s.pop();
         }
         s.push({ i, now });
     }
-
-    for (int i = 0; i < s.size(); i++) {
+  //오른쪽에 더 큰 수가 없다면 스택에서 제거되지 않고 남아있다
+  //그 수들의 오큰수는 -1이다(오큰수의 정의)
+    while (!s.empty()) {
         NGE[s.top().index] = -1;
         s.pop();
     }
-
+  
     for (int i = 0; i < n; i++) {
         cout << NGE[i] << ' ';
     }
