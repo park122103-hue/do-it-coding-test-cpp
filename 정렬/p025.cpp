0a2bbeea62f3c25083eff1ed23d1d50162564e4b
park122103-hue <park122103@gmail.com>
2026-07-12 11:45:05 +0900

    Create p025.cpp

---
+++ 정렬/p025.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+static vector<vector<int>> edge;
+static vector<bool> visited;
+static bool arrive;
+
+void dfs(int v, int depth);
+
+//신기한 소수 찾기 (백준 2023)
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+
+	edge.resize(N);
+	visited = vector<bool>(N, false);
+	arrive = false;
+	for (int i = 0; i < M; i++) {
+		int v, w;
+		cin >> v >> w;
+		edge[v].push_back(w);
+		edge[w].push_back(v);
+	}
+
+	for (int i = 0; i < N; i++) {
+		if (!arrive) {
+			dfs(i, 1);
+		}
+	}
+	cout << arrive;
+	return 0;
+}
+
+void dfs(int v, int depth) {
+	//문제 조건 만족 확인
+	if (depth == 5 || arrive) {
+		arrive = true;
+		return;
+	}
+	//현재 노드 방문
+	visited[v] = true;
+	//인접 노드, 깊이+1 탐색
+	while (!edge[v].empty()) {
+		int w = edge[v].back();
+		edge[v].pop_back();
+		dfs(w, depth + 1);
+	}
+	//인접 노드를 탐색후 돌아올 때 
+	visited[v] = false;
+}
